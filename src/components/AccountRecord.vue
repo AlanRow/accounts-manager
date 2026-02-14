efc677c26d3047b94de63c00bbcd5b22e65ccaaa
AlanRow <alekroga@gmail.com>
2026-02-14 13:47:13 +0300

    update styles

--- src/components/AccountRecord.vue
+++ src/components/AccountRecord.vue
@@ -1,57 +1,65 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from "vue"
-import { ElButton, ElInput, ElSelect, ElOption, ElFormItem, ElForm, type FormRules } from "element-plus"
-import { Delete as DeleteIcon } from "@element-plus/icons-vue"
+import { computed, reactive, ref } from 'vue'
+import {
+  ElButton,
+  ElInput,
+  ElSelect,
+  ElOption,
+  ElFormItem,
+  ElForm,
+  type FormRules,
+} from 'element-plus'
+import { Delete as DeleteIcon } from '@element-plus/icons-vue'
 
-import type { Account, AccountFormData, AccountType } from "@/types"
-import { convertAccountToFormData, convertFormDataToAccount } from "@/utils"
+import type { Account, AccountFormData, AccountType } from '@/types'
+import { convertAccountToFormData, convertFormDataToAccount } from '@/utils'
 
-import AccountRow from "./AccountRow.vue"
+import AccountRow from './AccountRow.vue'
 
 const ACCOUT_TYPE_LABELS: Record<AccountType, string> = {
-  "local": "Локальная",
-  "ldap": "LDAP",
+  local: 'Локальная',
+  ldap: 'LDAP',
 }
 
 const props = defineProps<{
-  modelValue: Account;
+  modelValue: Account
 }>()
 
 const emit = defineEmits<{
-  (e: "update:modelValue", value: Account): void;
-  (e: "remove"): void;
+  (e: 'update:modelValue', value: Account): void
+  (e: 'remove'): void
 }>()
 
-const formData = reactive<AccountFormData>(
-  convertAccountToFormData(props.modelValue)
-)
+const formData = reactive<AccountFormData>(convertAccountToFormData(props.modelValue))
 
-const hasPassword = computed(() => formData.type == "local")
+const hasPassword = computed(() => formData.type == 'local')
 
 /** по мере разрастания кода можно вынести эти правила в отдельный файл, но пока это не рационально */
 const rules = reactive<FormRules<AccountFormData>>({
   marks: [
-    { required: true, message: "Обязательно для заполнения", trigger: "blur" },
-    { max: 50, message: "Максимальная длина 50 символов", trigger: "blur" },
+    { required: true, message: 'Обязательно для заполнения', trigger: 'blur' },
+    { max: 50, message: 'Максимальная длина 50 символов', trigger: 'blur' },
     {
       validator: (rule, value, callback) => {
         if (!/^[a-zA-Z0-9_]+(;[a-zA-Z0-9_]+)*$/.test(value)) {
-          callback(new Error("Метки должны содержать символы A-Z, a-z, 0-9 и _, разделенные ;"))
+          callback(new Error('Метки должны содержать символы A-Z, a-z, 0-9 и _, разделенные ;'))
         } else {
           callback()
         }
       },
-      trigger: "blur"
-    }
+      trigger: 'blur',
+    },
   ],
   login: [
-    { required: true, message: "Обязательно для заполнения", trigger: "blur" },
-    { max: 100, message: "Максимальная длина 100 символов", trigger: "blur" },
+    { required: true, message: 'Обязательно для заполнения', trigger: 'blur' },
+    { max: 100, message: 'Максимальная длина 100 символов', trigger: 'blur' },
   ],
-  password: hasPassword.value ? [
-    { required: true, message: "Обязательно для заполнения", trigger: "blur" },
-    { max: 100, message: "Максимальная длина 100 символов", trigger: "blur" },
-  ] : [],
+  password: hasPassword.value
+    ? [
+        { required: true, message: 'Обязательно для заполнения', trigger: 'blur' },
+        { max: 100, message: 'Максимальная длина 100 символов', trigger: 'blur' },
+      ]
+    : [],
 })
 
 const formEl = ref<InstanceType<typeof ElForm> | null>(null)
@@ -60,7 +68,7 @@ function submitForm() {
   formEl.value?.validate((valid) => {
     if (valid) {
       const updatedAccount = convertFormDataToAccount(props.modelValue.id, formData)
-      emit("update:modelValue", updatedAccount)
+      emit('update:modelValue', updatedAccount)
     }
   })
 }
@@ -77,8 +85,13 @@ function submitForm() {
       </template>
       <template #type>
         <ElFormItem prop="type">
-          <ElSelect v-model="formData.type" placeholder="Тип учетной записи"  @change="submitForm">
-            <ElOption v-for="(label, type) in ACCOUT_TYPE_LABELS" :key="type" :value="type" :label="label" />
+          <ElSelect v-model="formData.type" placeholder="Тип учетной записи" @change="submitForm">
+            <ElOption
+              v-for="(label, type) in ACCOUT_TYPE_LABELS"
+              :key="type"
+              :value="type"
+              :label="label"
+            />
           </ElSelect>
         </ElFormItem>
       </template>
@@ -89,7 +102,13 @@ function submitForm() {
       </template>
       <template #password>
         <ElFormItem prop="password">
-          <ElInput v-model="formData.password" placeholder="Пароль" type="password" show-password @change="submitForm" />
+          <ElInput
+            v-model="formData.password"
+            placeholder="Пароль"
+            type="password"
+            show-password
+            @change="submitForm"
+          />
         </ElFormItem>
       </template>
       <template #actions>
